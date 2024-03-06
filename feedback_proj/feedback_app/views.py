cee6198110b41bf1cc6c754bd5d3d0ee757befd0
Mukilan Krishnakumar <mukilan.k@supporthive.com>
2024-03-06 13:52:15 +0530

    Email Functionality Added - Backend Complete

--- feedback_proj/feedback_app/views.py
+++ feedback_proj/feedback_app/views.py
@@ -1,11 +1,22 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.core.mail import EmailMessage
 
 
 # Create your views here.
 def feedback(request):
     if request.method == "POST":
-        print(request.POST)
+        data = request.POST
+        email = data["email"]
+        message = data["message"]
+        mail_subject = "Thanks for the feedback"
+        mail_message = "Thanks for the feedback"
+        email_obj = EmailMessage(mail_subject, mail_message, to=[email])
+        if email_obj.send():
+            print("Success: Email sent successfully")
+        else:
+            print("Failure: Email lost its way :(")
+        return redirect("thankyou")
     return render(request, "feedback_app/feedback.html")
 
 
