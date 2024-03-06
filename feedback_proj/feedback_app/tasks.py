1f96a687c7aad26015a148a5404d0996538956a2
Mukilan Krishnakumar <mukilan.k@supporthive.com>
2024-03-06 16:44:11 +0530

    Added Time Delay

--- feedback_proj/feedback_app/tasks.py
+++ feedback_proj/feedback_app/tasks.py
@@ -1,9 +1,11 @@
 from django.core.mail import EmailMessage
 from celery import shared_task
+from time import sleep
 
 
 @shared_task()
 def send_email(to_email):
+    sleep(10)
     mail_subject = "Thanks for the feedback"
     mail_message = "Thanks for the feedback"
     email_obj = EmailMessage(mail_subject, mail_message, to=[to_email])
